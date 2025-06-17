6f376a6b5fb69db1488730b18c7b0e7e73cd7ba4
Vikneswaran Venujan <UT010034@unicomtic.com>
2025-06-17 12:12:53 +0530

    8th

--- UnicomticManagmentsysytem/Views/LoginForm.Designer.cs
+++ UnicomticManagmentsysytem/Views/LoginForm.Designer.cs
@@ -190,6 +190,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(771, 450);
             this.Controls.Add(this.chkShowPassword);
             this.Controls.Add(this.button1);
